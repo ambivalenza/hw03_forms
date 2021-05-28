93674f7be087edb4e65d669119d6c9e0540535d0
Lex Shornikov <lex.shornikov@yandex.ru>
2021-05-28 22:05:58 +0300

    refresh

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
+from yatube.settings import COUNT_OF_PAGES
 
 from .forms import PostForm
 from .models import Group, Post, User
@@ -9,7 +10,8 @@ from .models import Group, Post, User
 def index(request):
     page_number = request.GET.get('page')
     post_list = Post.objects.all()
-    paginator = Paginator(post_list, 10)
+
+    paginator = Paginator(post_list, COUNT_OF_PAGES)
     page = paginator.get_page(page_number)
     return render(
         request,
@@ -24,12 +26,13 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     page_number = request.GET.get('page')
     post_list = Post.objects.all()
-    paginator = Paginator(post_list, 10)
+    paginator = Paginator(post_list, COUNT_OF_PAGES)
     page = paginator.get_page(page_number)
-    return render(request, "group.html",
+    return render(request,
+                  'group.html',
                   {
-                      "group": group,
-                      "page": page
+                      'group': group,
+                      'page': page
                   }
                   )
 
@@ -37,7 +40,7 @@ def group_posts(request, slug):
 def profile(request, username):
     author = get_object_or_404(User, username=username)
     posts = author.posts.all()
-    paginator = Paginator(posts, 10)
+    paginator = Paginator(posts, COUNT_OF_PAGES)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
     return render(
@@ -59,6 +62,7 @@ def post_view(request, username, post_id):
 
 @login_required()
 def post_edit(request, username, post_id):
+    is_edit = 1
     post = get_object_or_404(Post, id=post_id, author__username=username)
     if post.author != request.user:
         return redirect('post', username=username, post_id=post_id)
@@ -68,15 +72,27 @@ def post_edit(request, username, post_id):
         post.author = request.user
         post.save()
         return redirect('post', username=username, post_id=post_id)
-    return render(request, 'new.html', {'form': form, 'post_id': post_id})
+    return render(request, 'new.html',
+                  {
+        'form': form,
+        'post_id': post_id,
+        'is_edit' : is_edit
+    }
+                  )
 
 
 @login_required
 def new_post(request):
+    is_edit = 0
     form = PostForm(request.POST or None)
     if form.is_valid():
         post = form.save(commit=False)
         post.author = request.user
         post.save()
         return redirect('index')
-    return render(request, 'new.html', {'form': form})
+    return render(request, 'new.html',
+                  {
+                      'form': form,
+                      'is_edit': is_edit,
+                  }
+                  )
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -94,3 +94,5 @@ LOGIN_REDIRECT_URL = "index"
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+
+COUNT_OF_PAGES = 10
